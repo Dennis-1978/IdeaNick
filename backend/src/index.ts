18424988a4e59cc1bf533d305e07d4cfbaea803b
Khokhlov Denis <d.khokhloff2011@gmail.com>
2025-03-05 20:41:26 +0300

    Created express application

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,29 @@
-console.log('hello');
+import express from 'express';
+
+const ideas = [
+  {
+    nick: 'cool-idea-nick-1',
+    name: 'Idea 1',
+    description: 'Description of idea 1...',
+  },
+  {
+    nick: 'cool-idea-nick-2',
+    name: 'Idea 2',
+    description: 'Description of idea 2...',
+  },
+  {
+    nick: 'cool-idea-nick-3',
+    name: 'Idea 3',
+    description: 'Description of idea 3..',
+  },
+];
+
+const expressApp = express();
+
+expressApp.get('/ideas', (req, res) => {
+  res.send(ideas);
+});
+
+expressApp.listen(3000, () => {
+  console.info('Listening at http://localhost:3000');
+});
--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,22 +1,4 @@
 export function App() {
-  const ideas = [
-    {
-      nick: 'cool-idea-nick-1',
-      name: 'Idea 1',
-      description: 'Description of idea 1...',
-    },
-    {
-      nick: 'cool-idea-nick-2',
-      name: 'Idea 2',
-      description: 'Description of idea 2...',
-    },
-    {
-      nick: 'cool-idea-nick-3',
-      name: 'Idea 3',
-      description: 'Description of idea 3..',
-    },
-  ];
-
   return (
     <div>
       <h1>Ideanick</h1>
