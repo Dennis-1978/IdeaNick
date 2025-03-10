9292cbbf11d6348b04c4ce88771d43671557c5ff
Khokhlov Denis <d.khokhloff2011@yandex.ru>
2025-03-10 15:10:35 +0300

    Added check lint in webapp

---
+++ webapp/eslint.config.js
@@ -0,0 +1,30 @@
+import baseConfig from '../eslint.config.js';
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default [
+  ...baseConfig,
+
+  {
+    files: ['**/*.{ts,tsx,js,jsx}'],
+    languageOptions: {
+      parserOptions: {
+        project: ['tsconfig.json', 'tsconfig.node.json', 'tsconfig.app.json'],
+      },
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+
+  {
+    ignores: ['dist', 'node_modules', 'coverage', 'eslint.config.js'],
+  },
+  {
+    files: ['./vite.config.ts'],
+    languageOptions: {
+      parserOptions: {
+        project: ['tsconfig.json', 'tsconfig.node.json', 'tsconfig.app.json'],
+      },
+    },
+  },
+];
