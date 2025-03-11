0e1cc79d0875772e72b90678f12109a07679365f
Khokhlov Denis <d.khokhloff2011@yandex.ru>
2025-03-11 15:31:31 +0300

    Test commit husky again

--- backend/src/trpc.ts
+++ backend/src/trpc.ts
@@ -20,8 +20,6 @@ const ideas = [
 
 const trpc = initTRPC.create();
 
-const x: string = 1;
-
 export const trpcRouter = trpc.router({
   getIdeas: trpc.procedure.query(() => {
     return { ideas };
