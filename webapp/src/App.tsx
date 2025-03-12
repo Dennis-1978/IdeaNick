a12988651735658a65d8425debb21c3dbd7219ec
Khokhlov Denis <d.khokhloff2011@yandex.ru>
2025-03-12 20:17:54 +0300

    Route refactoring

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -2,7 +2,11 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { TrpcProvider } from './lib/trpc';
 import { AllIdeasPage } from './pages/AllIdeasPage';
 import { ViewIdeaPage } from './pages/ViewIdeaPage';
-import { getAllIdeasRoute, getViewIdeaRoute } from './lib/routes';
+import {
+  getAllIdeasRoute,
+  getViewIdeaRoute,
+  viewIdeaRouteParams,
+} from './lib/routes';
 
 export const App = () => {
   return (
@@ -11,7 +15,7 @@ export const App = () => {
         <Routes>
           <Route path={getAllIdeasRoute()} element={<AllIdeasPage />} />
           <Route
-            path={getViewIdeaRoute({ ideaNick: ':ideaNick' })}
+            path={getViewIdeaRoute(viewIdeaRouteParams)}
             element={<ViewIdeaPage />}
           />
         </Routes>
