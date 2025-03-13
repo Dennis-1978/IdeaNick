336e3b7e61ca1d2cd174dcc37e31b2b025254030
Khokhlov Denis <d.khokhloff2011@yandex.ru>
2025-03-13 11:13:38 +0300

    Created a Layout component and created a route

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -7,17 +7,20 @@ import {
   getViewIdeaRoute,
   viewIdeaRouteParams,
 } from './lib/routes';
+import { Layout } from './components/Layout';
 
 export const App = () => {
   return (
     <TrpcProvider>
       <BrowserRouter>
         <Routes>
-          <Route path={getAllIdeasRoute()} element={<AllIdeasPage />} />
-          <Route
-            path={getViewIdeaRoute(viewIdeaRouteParams)}
-            element={<ViewIdeaPage />}
-          />
+          <Route element={<Layout />}>
+            <Route path={getAllIdeasRoute()} element={<AllIdeasPage />} />
+            <Route
+              path={getViewIdeaRoute(viewIdeaRouteParams)}
+              element={<ViewIdeaPage />}
+            />
+          </Route>
         </Routes>
       </BrowserRouter>
     </TrpcProvider>
