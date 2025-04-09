1e1fbc12c2bd6d8d94a974047abc37c20f5a42d2
Khokhlov Denis <d.khokhloff2011@yandex.ru>
2025-04-09 11:31:47 +0300

    Transferring a Prisma client to a tRPC context

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,13 +2,25 @@ import { applyTrpcToExpressApp } from './lib/trpc';
 import cors from 'cors';
 import express from 'express';
 import { trpcRouter } from './router';
+import { AppContext, createAppContext } from './lib/ctx';
 
-const expressApp = express();
+void (async () => {
+  let ctx: AppContext | null = null;
 
-expressApp.use(cors());
+  try {
+    ctx = createAppContext();
 
-applyTrpcToExpressApp(expressApp, trpcRouter);
+    const expressApp = express();
 
-expressApp.listen(3000, () => {
-  console.info('Listening at http://localhost:3000');
-});
+    expressApp.use(cors());
+
+    applyTrpcToExpressApp(expressApp, ctx, trpcRouter);
+
+    expressApp.listen(3000, () => {
+      console.info('Listening at http://localhost:3000');
+    });
+  } catch (error) {
+    console.error(error);
+    await ctx?.stop();
+  }
+})();
