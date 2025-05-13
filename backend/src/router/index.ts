9a95b0aa8222462ad699add0c9eadfb5ad237ea9
Khokhlov Denis <d.khokhloff2011@yandex.ru>
2025-05-13 14:36:40 +0300

    Authorization: user model + registration endpoint

--- backend/src/router/index.ts
+++ backend/src/router/index.ts
@@ -3,11 +3,13 @@ import { trpc } from '../lib/trpc';
 import { getIdeaTrpcRoute } from './getIdea';
 import { getIdeasTrpcRoute } from './getIdeas';
 import { createIdeaTrpcRoute } from './createIdea';
+import { signUpTrpcRoute } from './signUp';
 
 export const trpcRouter = trpc.router({
   createIdea: createIdeaTrpcRoute,
   getIdeas: getIdeasTrpcRoute,
   getIdea: getIdeaTrpcRoute,
+  signUp: signUpTrpcRoute,
 });
 
 export type TrpcRouter = typeof trpcRouter;
