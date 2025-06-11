a8d6c483b9234d090e580bcd96806e7e23e5e676
Khokhlov Denis <d.khokhloff2011@yandex.ru>
2025-06-11 13:31:48 +0300

    Prisma: User and Idea entities are related

--- webapp/src/pages/ViewIdeaPage/index.tsx
+++ webapp/src/pages/ViewIdeaPage/index.tsx
@@ -31,6 +31,7 @@ export const ViewIdeaPage = () => {
       <div className={css.createdAt}>
         Created At: {format(data.idea.createdAt, 'yyyy-MM-dd')}
       </div>
+      <div className={css.author}>Auithor: {data.idea.author.nick}</div>
       <div
         className={css.text}
         dangerouslySetInnerHTML={{ __html: data.idea.text }}
